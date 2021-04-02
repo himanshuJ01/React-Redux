3c2b3f56ee43aba9253faadd3bd3a33136f87890
= <=>
2021-04-02 12:26:30 +0530

    react redux example with useSelect and use Dispatch

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
 import React from 'react';
 import './App.css';
-import { connect } from 'react-redux';
-import { anotherName} from './actions/myaction'
+import { anotherName} from './actions/myaction';
+import {useSelector,useDispatch} from 'react-redux';
+
 
 
 function App(props){
    
-   const mywish = props.mywish.map(item => {
+   const dispatch = useDispatch()
+   
+
+  const data = useSelector((state)=>{
+     return state
+   })
+
+  
+   const mywish = data.wish.map(item => {
       return <h2 key={Math.random()}>{item}</h2>
    })
+
    return(
       <div className="App">
-      <h1>Name {props.myname} </h1>
+      <h1>Name {data.name} </h1>
       {mywish}
-     <button onClick={()=>{props.changeName()}}>Change name</button> 
+     <button onClick={()=>{dispatch(anotherName())}}>Change name</button> 
      </div>
      
     
    );
    }
 
-   const mapStateToProps = (state) =>{
-      return {
-         myname: state.name,
-         mywish: state.wish
-      }
-   }
+   // const mapStateToProps = (state) =>{
+   //    return {
+   //       myname: state.name,
+   //       mywish: state.wish
+   //    }
+   // }
 
-   const mapDispatchToProps = (dispatch) => {
-      return {
-        changeName : () => {dispatch(anotherName())}
-      }
-   }
+   // const mapDispatchToProps = (dispatch) => {
+   //    return {
+   //      changeName : () => {dispatch(anotherName())}
+   //    }
+   // }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
